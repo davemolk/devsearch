bdedbbe2beaec281c9e52d8e879f2898bc67be85
davemolk <davemolk@gmail.com>
2021-07-14 04:53:23 -0600

    database built

--- projects/views.py
+++ projects/views.py
@@ -1,36 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 # Create your views here.
 
 
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
-
 def projects(request):
-    msg = 'Hello there'
-    number = 12
-    context = {'msg': msg, 'number': number, 'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
-
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+    projectObj = Project.objects.get(id = pk)
+    tags = projectObj.tags.all()
+    return render(request, 'projects/single-project.html', {'project': projectObj, 'tags': tags})
