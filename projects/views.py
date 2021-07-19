714aca252d3533c86d65eea01bb9132702cfb700
davemolk <davemolk@gmail.com>
2021-07-19 14:44:32 -0600

    redid projects.html

--- projects/views.py
+++ projects/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render, redirect
 from .models import Project
 from .forms import ProjectForm
 from django.contrib.auth.decorators import login_required
-from .utils import searchProjects
+from .utils import searchProjects, paginateProjects
+
+
 # Create your views here.
 
 
 def projects(request):
     projects, search_query = searchProjects(request)
-    context = {'projects': projects, 'search_query': search_query}
+    custom_range, projects = paginateProjects(request, projects, 6)
+    
+    context = {'projects': projects, 'search_query': search_query, 'custom_range': custom_range}
     return render(request, 'projects/projects.html', context)
 
 
