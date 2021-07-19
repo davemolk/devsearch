5ff026f8175ec4d34a07d45ee1c7fcccd7d6f3b6
davemolk <davemolk@gmail.com>
2021-07-18 20:06:13 -0600

    user actions done

--- projects/views.py
+++ projects/views.py
@@ -29,7 +29,7 @@ def createProject(request):
             project = form.save(commit=False)
             project.owner = profile
             project.save()
-            return redirect('projects')
+            return redirect('account')
 
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
@@ -45,7 +45,7 @@ def updateProject(request, pk):
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
-            return redirect('projects')
+            return redirect('account')
 
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
@@ -57,6 +57,6 @@ def deleteProject(request, pk):
     project = profile.project_set.get(id=pk)
     if request.method == 'POST':
         project.delete()
-        return redirect('projects')
+        return redirect('account')
     context = {'object': project}
     return render(request, 'delete_template.html', context)
